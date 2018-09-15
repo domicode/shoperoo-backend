9feaaff3a6c20a2fa938c83284b1850befc15b8f
Dominique Schuwey <domi@domidesign.ch>
2018-09-15 23:02:15 +0200

    add nested attributes

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all.includes(:substitutes)
-    render json: @products.to_json(include: [:substitutes], methods: [
+    render json: @products.to_json(include: [:substitutes => {methods: [      :has_season?,
+      :water_score,
+      :co2_score,
+      :people_score,
+      :energy_score,
+      :total_score]}], methods: [
       :has_season?,
       :water_score,
       :co2_score,
