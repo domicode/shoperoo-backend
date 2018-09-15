3506823beed883ad9445e2abd17cf3b978ac60e8
Dominique Schuwey <domi@domidesign.ch>
2018-09-15 19:37:01 +0200

    feat: add product scores

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::ProductsController < ApplicationController
   def index
     @products = Product.all
-    render json: @products.to_json(methods: [:substitute, :has_season?])
+    render json: @products.to_json(methods: [
+      :substitute,
+      :has_season?,
+      :water_score,
+      :co2_score,
+      :people_score,
+      :energy_score,
+      :total_score
+    ])
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,40 @@ class Product < ApplicationRecord
     month = Date.today.strftime("%B").downcase
     seasons.where(month, true).present?
   end
+
+  def co2_score
+    if co2_usage.present?
+      100 - co2_usage
+    else
+      0
+    end
+  end
+
+  def water_score
+    if water_usage.present?
+      100 - water_usage
+    else
+      0
+    end
+  end
+
+  def people_score
+    if people_usage.present?
+      100 - people_usage
+    else
+      0
+    end
+  end
+
+  def energy_score
+    if energy_usage.present?
+      100 - energy_usage
+    else
+      0
+    end
+  end
+
+  def total_score
+    co2_score + water_score + people_score + energy_score
+  end
 end
